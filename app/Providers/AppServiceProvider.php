00f9995f3299f3be19a2c15d6ace5626c1ae8835
laguyu <wilmar88880@gmail.com>
2026-06-08 21:55:46 -0400

    test vercel 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Throwable;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+          if (isset($_ENV['VERCEL'])) {
+            // Creamos las carpetas dinámicamente en el directorio temporal si no existen
+            $paths = [
+                '/tmp/storage/framework/views',
+                '/tmp/storage/framework/cache',
+                '/tmp/storage/framework/sessions',
+                '/tmp/storage/bootstrap/cache'
+            ];
+            foreach ($paths as $path) {
+                if (!is_dir($path)) {
+                    mkdir($path, 0755, true);
+                }
+            }
+
+            // Reconfiguramos las rutas de guardado en caliente
+            Config::set('view.compiled', '/tmp/storage/framework/views');
+            Config::set('cache.stores.file.path', '/tmp/storage/framework/cache');
+            Config::set('session.files', '/tmp/storage/framework/sessions');
+        }
+
+
         app()->setLocale('es');
 
         if (app()->environment('production') || env('FORCE_HTTPS', false)) {
